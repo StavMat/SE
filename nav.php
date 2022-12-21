715ce05265c7ac96dcec125c5d5676b5881a6767
StavMat <116916502+StavMat@users.noreply.github.com>
2022-12-22 00:48:35 +0100

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,21 @@
+<?php
+  echo "<p> ktora helikoptera je lepsia:";
+  echo $NA;
+  
+  echo "<p> Bola tato tema zaujimava?:";
+  echo $PA;
+  
+  echo "<p> Mysliťe si že je táto helikoptéra hodná jej reputacie?:";
+  echo $DA;
+  
+  echo "<p>  Ktore kontinenty pouzivaju UH-60?:";
+  echo $Stranka;
+  
+ 
+  
+  echo "<p> Ako by ste ohodnotily tuto pracu:";
+  echo $Hodnotenie;
+  
+  ?>
+
+  
